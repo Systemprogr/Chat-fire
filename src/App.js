e96b3320595d336ea9874ae921d829e2a45728bd
David <davidcq189@gmail.com>
2022-12-22 19:17:38 -0500

    cambio color

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useRef, useState, useEffect} from 'react';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import "./App.css";
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection,  query, orderBy, limit, serverTimestamp, addDoc } from 'firebase/firestore';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  limit,
+  serverTimestamp,
+  addDoc,
+} from "firebase/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 //import 'firebase/compat/analytics';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const firebaseConfig = {
-  // your config
+  apiKey: "AIzaSyDwCblXotQZt0nF9AtPrd09tXhMFisldVo",
+  authDomain: "test1firebase-a1f4e.firebaseapp.com",
+  projectId: "test1firebase-a1f4e",
+  storageBucket: "test1firebase-a1f4e.appspot.com",
+  messagingSenderId: "237925032745",
+  appId: "1:237925032745:web:749d5f3f0668ed1f5c0d38",
+  measurementId: "G-GPJWECEY8P",
 };
 
 initializeApp(firebaseConfig);
@@ -19,58 +33,56 @@ const db = getFirestore();
 const auth = getAuth();
 //const analytics = firebase.analytics();
 
-
 function App() {
-
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>Firebase Chat </h1>
         <SignOut />
       </header>
 
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
 
 function SignIn() {
-
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-  }
+    signInWithPopup(auth, provider);
+  };
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Iniciar con Google
+      </button>
+      <p>Chat en tiempo real</p>
     </>
-  )
-
+  );
 }
 
 function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Cerrar Sesion
+      </button>
+    )
+  );
 }
-
 
 function ChatRoom() {
   const dummy = useRef();
   const messageInput = useRef();
-  const messagesRef = collection(db, 'messages');
-  const q = query(messagesRef, orderBy('createdAt', 'desc'), limit(25));
+  const messagesRef = collection(db, "messages");
+  const q = query(messagesRef, orderBy("createdAt", "desc"), limit(25));
 
   const [messages] = useCollectionData(q);
 
-  const [formValue, setFormValue] = useState('');
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -81,51 +93,65 @@ function ChatRoom() {
       uid,
       photoURL,
       text: formValue,
-      createdAt: serverTimestamp()
-    })
+      createdAt: serverTimestamp(),
+    });
 
     messageInput.current.focus();
-    setFormValue('');
+    setFormValue("");
     //dummy is updated when the component is re-rendered, instead of after a message is sent.
-  }
+  };
 
   //Everytime messages is modified, will use the dummy reference to scroll to the bottom.
   useEffect(() => {
-    dummy.current?.scrollIntoView({ behavior: 'smooth' });
+    dummy.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
-  return (<>
-    <main>
+  return (
+    <>
+      <main>
+        {messages &&
+          messages
+            .slice(0)
+            .reverse()
+            .map((msg, idx) => <ChatMessage key={idx} message={msg} />)}
 
-      {messages && messages.slice(0).reverse().map((msg, idx) => <ChatMessage key={idx} message={msg} />)}
-    
-      <div ref={dummy}/>
+        <div ref={dummy} />
+      </main>
 
-    </main>
+      <form onSubmit={sendMessage}>
+        <input
+          ref={messageInput}
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Escribe......"
+        />
 
-    <form onSubmit={sendMessage}>
-
-      <input ref={messageInput} value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
+        <button type="submit" disabled={!formValue}>
+          Enviar 😁
+        </button>
+      </form>
+    </>
+  );
 }
-
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} referrerPolicy="no-referrer" />
-      <p>{text}</p>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+          referrerPolicy="no-referrer"
+        />
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
-
 
 export default App;
